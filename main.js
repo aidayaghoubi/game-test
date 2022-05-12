5c13cadddffc181adfbefe5619440a8e7cc1b2e2
ali jahangiri <jahangiri.dev@gmail.com>
2022-05-12 23:31:52 +0430

    re-sync

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 "use strict";
 
-import { test } from "./utils/helper.js";
+import { debounce } from "./utils/helper.js";
 
 const playground = document.getElementById("playground");
 const playerContainer = document.getElementById("player");
+const characterImage = playerContainer.getElementsByTagName("img")[0]
 
 
-playground.addEventListener("click" , function handleCharacterMovement(e) {
-    console.log(e);
+// playground.addEventListener("click" , function handleCharacterMovement(e) {
+//     console.log(e);
+// });
+
+// playground.addEventListener("click" , function handleCharacterShotting(e) {
+//     console.log(e.target);
+// });
+
+playground.addEventListener("mousemove", function handleUserCursorChange(e) {
+
+    const axises = {
+        x: e.clientX,
+        y: e.clientY
+    }
+    
+    const endDegrees = Math.atan(axises.x / axises.y) * 180 / Math.PI;
+    
+    playerContainer.style.transform = `rotate(${(endDegrees - 180) * -1}deg)`;
 });
 
-playground.addEventListener("click" , function handleCharacterShotting(e) {
-    console.log(e.target);
-});
